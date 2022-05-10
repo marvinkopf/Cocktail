cf86e91f71fa9eba7e167ff687181fb0c456098f
Oliver Brewka <pymeonlee@gmail.com>
2022-05-10 13:58:25 +0200

    initial models

--- data/web/api/serializers.py
+++ data/web/api/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 
-from main.models import Beispiel
+from main.models import Beispiel, Rezept, Zutat
 
 
 class BeispielSerializer(serializers.ModelSerializer):
     class Meta:
         model = Beispiel
         fields = ['name']
+
+
+class RezeptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rezept
+        fields = ['name', 'imgUrl', 'instructions']
+
+class ZutatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Zutat
+        fields = ['name', 'unit', 'recipes']
+
+
+    
 
