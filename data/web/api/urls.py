fc93a2f9786f9170baf94291b40e1555896a4ed0
Oliver Brewka <pymeonlee@gmail.com>
2022-06-01 19:58:02 +0200

    urlpattern angepasst, gemäß Vorschlag aus Präsi

--- data/web/api/urls.py
+++ data/web/api/urls.py
@@ -3,7 +3,7 @@ from api import views
 
 
 urlpatterns = [
-    path('api/beispiel/', views.BeispielList.as_view()),
-    path('api/rezept/', views.RezeptList.as_view()),
-    path('api/zutat/', views.ZutatList.as_view()),
+    path("beispiel/", views.BeispielList.as_view()),
+    path("rezept/", views.RezeptList.as_view()),
+    path("zutat/", views.ZutatList.as_view()),
 ]
