0568f070a84fd8b23924d6b428bcd14f09b62dd3
Oliver Brewka <pymeonlee@gmail.com>
2022-06-02 17:37:03 +0200

    POPULATEDB command hinzugefügt und models angepasst.

--- data/web/api/serializers.py
+++ data/web/api/serializers.py
@@ -1,24 +1,28 @@
 from rest_framework import serializers
 
-from main.models import Beispiel, Rezept, Zutat
-
-
-class BeispielSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Beispiel
-        fields = ['name']
+from main.models import Rezept, Zutat, Rezept_Zutat
 
 
 class RezeptSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rezept
-        fields = ['name', 'imgUrl', 'instructions']
+        fields = [
+            "id",
+            "name",
+            "is_alcoholic",
+            "instruction_en",
+            "instruction_de",
+            "img_url",
+        ]
+
 
 class ZutatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Zutat
-        fields = ['name', 'unit', 'recipes']
+        fields = "__all__"
 
 
-    
-
+class Rezept_ZutatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rezept_Zutat
+        fields = "__all__"
